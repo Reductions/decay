4e09d74f38e5ed02da6d7e51daef77cc74e2a69c
Reductions <slavi.boyanov@gmail.com>
2017-01-23 05:13:22 +0200

    Initial commit

---
+++ lib/decay.ex
@@ -0,0 +1,18 @@
+defmodule Decay do
+  @moduledoc """
+  Documentation for Decay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Decay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/decay_test.exs
@@ -0,0 +1,8 @@
+defmodule DecayTest do
+  use ExUnit.Case
+  doctest Decay
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
